bd3f9f29ed76708e4a3d3519ba1e55c4de39f75a
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2022-08-02 21:23:08 +0530

    Kth Smallest Element in a Sorted Matrix
     Kth Smallest Element in a Sorted Matrix

---
+++ kth-smallest-element-in-a-sorted-matrix.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+int m, n;
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        m = matrix.size(), n = matrix[0].size(); // For general, the matrix need not be a square
+        int left = matrix[0][0], right = matrix[m-1][n-1], ans = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (countLessOrEqual(matrix, mid) >= k) {
+                ans = mid;
+                right = mid - 1; // try to looking for a smaller value in the left side
+            } else left = mid + 1; // try to looking for a bigger value in the right side
+        }
+        return ans;
+    }
+    int countLessOrEqual(vector<vector<int>>& matrix, int x) {
+        int cnt = 0, c = n - 1; // start with the rightmost column
+        for (int r = 0; r < m; ++r) {
+            while (c >= 0 && matrix[r][c] > x) --c;  // decrease column until matrix[r][c] <= x
+            cnt += (c + 1);
+        }
+        return cnt;
+    }
+
+     int main(){
+       
+         vector<vector<int>> vect
+    {
+        {1,5,9},{10,11,13},{12,13,15}
+    };
+        int k = 8;
+        cout<<kthSmallest(vect,k);
+    }
