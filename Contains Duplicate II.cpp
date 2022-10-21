355da0c568bce29f0dfc8d962a60b06424075500
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2022-10-21 21:43:13 +0530

    Contains Duplicate II
    Contains Duplicate II

---
+++ Contains Duplicate II.cpp
@@ -0,0 +1,2 @@
+
+bool containsNearbyDuplicate(vector<int> &nums, int k) { map<int, int> m; for (int i = 0; i < nums.size(); i++) { if (m.find(nums[i]) != m.end()) { if (abs(i + 1 - m[nums[i]]) <= k) { return true; } else { m[nums[i]] = i + 1; } } else { m[nums[i]] = i + 1; } } return false; }
