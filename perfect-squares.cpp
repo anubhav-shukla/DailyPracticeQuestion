f8e9ba5d56b296153b3e6c78acf5bae6412cf96e
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2022-11-22 20:55:12 +0530

    Perfect Squares
    Perfect Squares

---
+++ perfect-squares.cpp
@@ -0,0 +1,29 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+int numSquares(int n) {
+        vector<int> s;
+        vector<int> dp(n+1, INT_MAX);
+        for(int i = 1;(i*i)<=n;i++){
+            s.push_back(i*i);
+            dp[i*i] = 1;
+        }
+        dp[0] = 0;
+        for(int i=1;i<=n;i++){
+           for(auto &it: s){
+               if(i>=it){
+                   dp[i] = min(dp[i],1+dp[i-it]);
+               }
+           }
+        }
+        return dp[n];
+    }
+
+
+    int main(){
+        int n = 12;
+        cout<<numSquares(n);
+    }
