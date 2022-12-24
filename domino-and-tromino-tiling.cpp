3846abaf969e560977bcdce91fcd557254e3beb7
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2022-12-24 18:17:06 +0530

    Domino and Tromino Tiling
    Domino and Tromino Tiling

---
+++ domino-and-tromino-tiling.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int numTilings(int N) {
+        int a = 0, b = 1, c = 1, c2, mod = 1e9 + 7;
+        while (--N) {
+            c2 = (c * 2 % mod + a) % mod;
+            a = b;
+            b = c;
+            c = c2;
+        }
+        return c;
+    }
+
+
+    int main(){
+        cout<<numTilings(3);
+    }
