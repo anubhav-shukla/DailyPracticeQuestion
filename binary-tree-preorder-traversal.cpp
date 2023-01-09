4d741dbdfd579cf8eeb670da7f1dad139565a09d
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2023-01-09 21:20:37 +0530

    Binary Tree Preorder Traversal
    Binary Tree Preorder Traversal

---
+++ binary-tree-preorder-traversal.cpp
@@ -0,0 +1,9 @@
+vector<int>ans;
+    vector<int> preorderTraversal(TreeNode* root) {
+        if(root){
+            ans.push_back(root->val);
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);
+        }
+        return ans;
+    }
