e4ca543f88b02e073eb2caf0143b66b1a97a16dc
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2023-01-10 21:46:27 +0530

    Same Tree
    Same Tree

---
+++ same-tree.cpp
@@ -0,0 +1,24 @@
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+
+bool isSameTree(TreeNode* p, TreeNode* q)
+    {
+        stack<pair<TreeNode*, TreeNode*>> st;
+        st.push({p, q});
+        while (!st.empty()) {
+            auto [node1, node2] = st.top();
+            st.pop();
+            if (!node1 && !node2) continue;
+            if (!node1 || !node2 || node1->val != node2->val) return false;
+            st.push({node1->left, node2->left});
+            st.push({node1->right, node2->right});
+        }
+        return true;
+    }
