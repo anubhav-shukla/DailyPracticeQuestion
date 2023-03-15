d6e3ac1e792bdac9017d4d872c8a5e0ee025a1b9
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2023-03-15 21:23:25 +0530

    Sum Root to Leaf Numbers
    Sum Root to Leaf Numbers

--- Sum Root to Leaf Numbers.cpp
+++ Sum Root to Leaf Numbers.cpp
@@ -1,14 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int sumNumbers(TreeNode* root, int sum=0) {
-        int ret=0;
-        sum=sum*10+root->val;
-        if(!root->left && !root->right)
-        {
-            ret+=sum;
+    int sumNumbers(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        int ret = 0;
+        stack<pair<TreeNode*, int>> s;
+        s.push({root, 0});
+        while(!s.empty()) {
+            auto it = s.top();
+            s.pop();
+            TreeNode* curr = it.first;
+            int curr_sum = it.second;
+            curr_sum = curr_sum * 10 + curr->val;
+            // 如果是叶子节点
+            if (curr->left == NULL && curr->right == NULL) {
+                ret += curr_sum;
+            }
+            if (curr->right != NULL) {
+                s.push({curr->right, curr_sum});
+            }
+            if (curr->left != NULL) {
+                s.push({curr->left, curr_sum});
+            }
         }
-        if(root->left) ret+=sumNumbers(root->left,sum);
-        if(root->right) ret+=sumNumbers(root->right,sum);
         return ret;
     }
 };
