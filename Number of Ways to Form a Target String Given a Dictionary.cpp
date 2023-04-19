573930259beeba72633b9cb7ddf90bdcb8f9cab4
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2023-04-19 20:18:03 +0530

    Number of Ways to Form a Target String Given a Dictionary
    Number of Ways to Form a Target String Given a Dictionary

--- Number of Ways to Form a Target String Given a Dictionary.cpp
+++ Number of Ways to Form a Target String Given a Dictionary.cpp
@@ -1,14 +1,35 @@
-int numWays(vector<string>& words, string target) {
-        int n = target.length(), mod = 1e9 + 7;
-        vector<long> res(n + 1);
-        res[0] = 1;
-        for (int i = 0; i < words[0].length(); ++i) {
-            vector<int> count(26);
-            for (auto& w : words)
-                count[w[i] - 'a']++;
-            for (int j = n - 1; j >= 0; --j) {
-                res[j + 1] += res[j] * count[target[j] - 'a'] % mod;
+class Solution {
+public:
+    //Dp and Memoization Solution,gets Accepted✅    
+    int mod=1e9+7;
+    int dp[1000][1001];   
+    int find(int id,vector<vector<int>>& fre,string& target,int col,int n,int m){
+        if(id==n){
+            return 1;
+        }
+        if(col==m){
+            return 0;
+        }
+        if(dp[id][col]!=-1){
+            return dp[id][col];
+        }
+        int not_pick=find(id,fre,target,col+1,n,m);
+        int pick=0;
+        if(fre[col][target[id]-'a']!=0){
+            int curr=fre[col][target[id]-'a'];
+            pick=(((long)curr*(long)find(id+1,fre,target,col+1,n,m)%mod)%mod)%mod;
+        }
+        return dp[id][col]=(pick +not_pick)%mod;
+    }
+    int numWays(vector<string>& words, string target) {
+        memset(dp,-1,sizeof(dp));
+        int n=target.size(),m=words[0].size();
+        vector<vector<int>> fre(m,vector<int>(26,0));
+        for(int i=0;i<words.size();i++){
+            for(int j=0;j<words[i].size();j++){
+                fre[j][words[i][j]-'a']++;
             }
         }
-        return res[n] % mod;
+        return find(0,fre,target,0,n,m);
     }
+};
