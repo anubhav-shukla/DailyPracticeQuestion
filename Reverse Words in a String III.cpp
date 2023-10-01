dc9bdcbfcac48db8fa50b049dd87ba20e66623c9
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2023-10-01 19:26:51 +0530

    Reverse Words in a String III
    Reverse Words in a String III

--- Reverse Words in a String III.cpp
+++ Reverse Words in a String III.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     string reverseWords(string s) {
         stringstream ss(s);
-        string t, ans;
-        
-        while (getline(ss, t, ' ')) {
-            reverse(t.begin(), t.end());
-            ans += t + ' ';
+        string temp,ans;
+        while(ss>>temp){
+            reverse(temp.begin(),temp.end());
+            ans+= (temp+" ");
         }
-        // We will take substring of one less size as there will be space after last word which is not required.
-        return ans.substr(0, ans.size() - 1); 
+        
+        return ans.substr(0,ans.size()-1);
     }
 };
