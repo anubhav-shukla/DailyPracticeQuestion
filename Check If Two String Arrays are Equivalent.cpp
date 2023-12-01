03b7aa58c46e90d5119f1c9818af40878099a9b6
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2023-12-01 19:57:49 +0530

    Check If Two String Arrays are Equivalent
    Check If Two String Arrays are Equivalent

--- Check If Two String Arrays are Equivalent.cpp
+++ Check If Two String Arrays are Equivalent.cpp
@@ -1,16 +1,22 @@
-bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string s1;
-        string s2;
-        for(int i=0;i<word1.size();i++)
-        {
-            s1+=word1[i];
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+
+        for(String word : word1){
+            str1.append(word);
         }
-        for(int i=0;i<word2.size();i++)
-        {
-            s2+=word2[i];
+        for(String word : word2){
+            str2.append(word);
         }
-        if(s1==s2)
+        String s1 = str1.toString();
+        String s2 = str2.toString();
+
+        if(s1.equals(s2)){
             return true;
-        else
+        }
+        else{
             return false;
+        }
     }
+}
