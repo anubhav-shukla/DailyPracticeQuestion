17d6db48f25c3c8bea89f3127d16a5228e9a6753
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2024-02-23 19:39:04 +0530

    Cheapest Flights Within K Stops
    Cheapest Flights Within K Stops

--- Cheapest Flights Within K Stops.cpp
+++ Cheapest Flights Within K Stops.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<vector<int>>> edge(100);
-        for (auto& f : flights)
-        {
-            edge[f[0]].push_back({f[1],f[2]});
+        unordered_map<int, vector<pair<int, int>>> adj;
+        for (auto& flight : flights) {
+            adj[flight[0]].push_back({flight[1], flight[2]});
         }
-        vector<int> visited(n,10000000);
-        queue<vector<int>> q;
-        visited[src] = 0;
-        q.push({src,0,0});
-        int ans = 10000000;
-        while(!q.empty())
-        {
-            int city  = q.front()[0];
-            int hop   = q.front()[2];
-            int price = q.front()[1];
-            q.pop();
-            for (auto &e : edge[city])
-            {
-                if (visited[e[0]] <= price+e[1]) continue;   // better price available
-                visited[e[0]] = price+e[1];                  // update base price for this ciry as hop
-                if (e[0] == dst) continue;  // reach the destination (still check for better price)
-                if (hop == k) continue;     // already k flights     (still check for better price)
-                 q.push({e[0],visited[e[0]],hop+1});
-            }            
+
+        vector<int> dist(n, INT_MAX);
+        dist[src] = 0;
+
+        queue<pair<int, int>> q;
+        q.push({src, 0});
+        int stops = 0;
+
+        while (!q.empty() && stops <= k) {
+            int sz = q.size();
+            while (sz-- > 0) {
+                auto [node, distance] = q.front();
+                q.pop();
+
+                if (!adj.count(node)) continue;
+
+                for (auto& [neighbour, price] : adj[node]) {
+                    if (price + distance >= dist[neighbour]) continue;
+                    dist[neighbour] = price + distance;
+                    q.push({neighbour, dist[neighbour]});
+                }
+            }
+            stops++;
         }
-        return (visited[dst] == 10000000)?-1:visited[dst];
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
     }
+};
