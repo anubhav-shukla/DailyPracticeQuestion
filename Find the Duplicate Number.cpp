9286e421a2d6d1b7d5fcce345dcdf3408e91a1eb
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2024-03-24 17:50:27 +0530

    Find the Duplicate Number
    Find the Duplicate Number

--- Find the Duplicate Number.cpp
+++ Find the Duplicate Number.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        map<int, int> m;
-        int duplicate = 0;
-        for(auto i= 0; i<nums.size(); ++i)
-        ++m[nums[i]];
-        
-        for(auto i : m)
-        {
-            if(i.second > 1)
-            {
-                duplicate = i.first;
-                break;
+    int findDuplicate(std::vector<int>& nums) {
+        int left = 1;
+        int right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = 0;
+
+            // Count the numbers less than or equal to mid
+            for (int num : nums) {
+                if (num <= mid) {
+                    count++;
+                }
+            }
+
+            // If count is greater than mid, the duplicate lies in the left half
+            if (count > mid) {
+                right = mid;
+            } else { // Otherwise, it lies in the right half
+                left = mid + 1;
             }
         }
-        return duplicate;
-    }
 
+        return left;
+    }
 };
