524ca866e1c779855e7075ce4c581f20ae7c588d
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2024-08-20 16:29:29 +0530

    Stone Game II

--- Stone Game II.cpp
+++ Stone Game II.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
-        vector<int> suffixSums(n + 1);
-        suffixSums[n - 1] = piles[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            suffixSums[i] = suffixSums[i + 1] + piles[i];
+        int length = piles.size();
+        vector<vector<int>> dp(length + 1, vector<int>(length + 1,0));
+        vector<int> sufsum (length + 1, 0);
+        for (int i = length - 1; i >= 0; i--) {
+            sufsum[i] = sufsum[i + 1] + piles[i];
         }
-
-        vector<vector<int>> dp(n, vector<int>(n + 1));
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int m = 1; m <= n; m++) {
-                if (i + 2 * m >= n) {
-                    dp[i][m] = suffixSums[i];
-                } else {
-                    for (int x = 1; x <= 2 * m; x++) {
-                        int opponentScore = dp[i + x][max(x, m)];
-                        int score = suffixSums[i] - opponentScore;
-                        dp[i][m] = max(dp[i][m], score);
-                    }
-                }
-            }
+        return helper(dp, sufsum, 0, 1);
+    }    
+    int helper(vector<vector<int>>& dp, vector<int>& sufsum, int i, int M) {
+        if (i == sufsum.size()) return 0;
+        if (2*M >= sufsum.size() - i) return sufsum[i];
+        if (dp[i][M] != 0) return dp[i][M];
+        int res = INT_MAX;
+        for (int X = 1; X <= 2*M; X++) {
+            res = min(res, helper(dp, sufsum, i+X, max(X,M)));
         }
-
-        return dp[0][1];
+        dp[i][M] = sufsum[i] - res;
+        return dp[i][M];
     }
 };
