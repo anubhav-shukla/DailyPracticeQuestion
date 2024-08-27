4dbdf7b7897535f6cce1a8e35fdb9300d56fb42b
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2024-08-27 16:47:38 +0530

    Path with Maximum Probability
    Path with Maximum Probability

--- Path with Maximum Probability.cpp
+++ Path with Maximum Probability.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
-
-        // Adjacency list
-        vector<vector<pair<int, double>>> adj(n);
-        for (int i = 0; i < edges.size(); i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            adj[u].push_back({v, succProb[i]});
-            adj[v].push_back({u, succProb[i]});
+    void dfs(vector<vector<pair<int, double>>>& graph, vector<bool>& vis, int src, int dst, double curres, double& res, double& mn) {
+        if (curres <= mn)
+            return;
+        if (src == dst) {
+            res = max(res, curres);
+            return;
         }
-
-        // ans will be in dist[end]
-        vector<double> dist(n, 0.0);
-        dist[start] = 1.0;
-        
-        queue<int> q;
-        q.push(start);
-        
-        while (!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            
-            for (auto x : adj[curr]) {
-                int node = x.first;
-                double prob = x.second;
-                double newProb = dist[curr] * prob;
-                
-                if (newProb > dist[node]) {
-                    dist[node] = newProb;
-                    q.push(node);
-                }
-            }
+        vis[src] = true;
+        for (const auto& cur : graph[src]) {
+            int newSrc = cur.first;  
+            double newSrcProb = cur.second; 
+            if (!vis[newSrc] && curres * newSrcProb >= res)
+                dfs(graph, vis, newSrc, dst, curres * newSrcProb, res, mn);
         }
-        
-        return dist[end];
+        vis[src] = false;  
+    }
+
+    double maxProbability(int n, vector<vector<int>>& e, vector<double>& succProb, int src, int dst) {
+        vector<vector<pair<int, double>>> graph(n);
+        for (int i = 0; i < e.size(); i++) {
+            graph[e[i][0]].push_back( { e[i][1], succProb[i] } );
+            graph[e[i][1]].push_back( { e[i][0], succProb[i] } );
+        }
+        vector<bool> vis(n, false);  
+        double res = 0, mn = 1e-5;
+        dfs(graph, vis, src, dst, 1.0, res, mn);
+        return res;
     }
 };
