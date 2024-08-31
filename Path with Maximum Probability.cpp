536df4d5ad71b4ac465bb8e7d5cc18e23310c114
Anubhav Shukla <43886210+anubhav-shukla@users.noreply.github.com>
2024-08-31 19:34:03 +0530

    Path with Maximum Probability
    Path with Maximum Probability

--- Path with Maximum Probability.cpp
+++ Path with Maximum Probability.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
-    void dfs(vector<vector<pair<int, double>>>& graph, vector<bool>& vis, int src, int dst, double curres, double& res, double& mn) {
-        if (curres <= mn)
-            return;
-        if (src == dst) {
-            res = max(res, curres);
-            return;
-        }
-        vis[src] = true;
-        for (const auto& cur : graph[src]) {
-            int newSrc = cur.first;  
-            double newSrcProb = cur.second; 
-            if (!vis[newSrc] && curres * newSrcProb >= res)
-                dfs(graph, vis, newSrc, dst, curres * newSrcProb, res, mn);
-        }
-        vis[src] = false;  
-    }
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+        unordered_map<int, vector<pair<int, double>>> adj;  // Adjacency list
+        vector<double> result(n, 0.0);  // To store the max probability to reach each node
 
-    double maxProbability(int n, vector<vector<int>>& e, vector<double>& succProb, int src, int dst) {
-        vector<vector<pair<int, double>>> graph(n);
-        for (int i = 0; i < e.size(); i++) {
-            graph[e[i][0]].push_back( { e[i][1], succProb[i] } );
-            graph[e[i][1]].push_back( { e[i][0], succProb[i] } );
+        // Build the adjacency list
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            double prob = succProb[i];
+            adj[u].push_back({v, prob});
+            adj[v].push_back({u, prob});
         }
-        vector<bool> vis(n, false);  
-        double res = 0, mn = 1e-5;
-        dfs(graph, vis, src, dst, 1.0, res, mn);
-        return res;
+
+        // Max-heap priority queue to store (probability, node)
+        priority_queue<pair<double, int>> pq;
+
+        result[start_node] = 1.0;  // Start node probability is 1
+        pq.push({1.0, start_node});
+
+        while (!pq.empty()) {
+            double currProb = pq.top().first;
+            int currNode = pq.top().second;
+            pq.pop();
+
+            // If we reached the end_node, return the probability
+            if (currNode == end_node) {
+                return currProb;
+            }
+
+            for (auto& neighbor : adj[currNode]) {
+                int adjNode = neighbor.first;
+                double adjProb = neighbor.second;
+                
+                // If we find a better probability, update it
+                if (result[adjNode] < currProb * adjProb) {
+                    result[adjNode] = currProb * adjProb;
+                    pq.push({result[adjNode], adjNode});
+                }
+            }
+        }
+
+        return result[end_node];  // Return the max probability to reach end_node
     }
 };
